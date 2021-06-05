a542f9c76fcdfc8deecb6b4487782f426373b881
SerhiiVakoliuk <s.vakoliuk@gmail.com>
2021-06-05 23:48:56 +0300

    05 July 2021

---
+++ src/app/modules/user/services/user.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {User} from "../models/User";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private url='https://jsonplaceholder.typicode.com/users';
+
+  constructor(private httpClient:HttpClient) { }
+
+  getUsers():Observable<User[]>{
+    return this.httpClient.get<User[]>(this.url);
+  }
+}
---
+++ src/app/modules/user/user.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule} from "@angular/common/http";
+import { UserRoutingModule } from './user-routing.module';
+
+import { UsersComponent } from './components/users/users.component';
+import {UserService} from "./services/user.service";
+import { UserComponent } from './components/user/user.component';
+
+
+@NgModule({
+  declarations: [
+    UsersComponent,
+    UserComponent
+  ],
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    HttpClientModule
+  ],
+  providers:[UserService]
+})
+export class UserModule { }
